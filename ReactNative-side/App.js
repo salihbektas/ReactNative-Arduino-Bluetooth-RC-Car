60dbaf8bbe7ea5b0b235abfe85c85998f9620d37
Salih Bektaş <sbektas@gtu.edu.tr>
2022-09-12 15:43:36 +0300

    interface simplified

--- ReactNative-side/App.js
+++ ReactNative-side/App.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   function pressLeft(paramater){
@@ -20,73 +20,37 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={{flex: 1, width:"100%"}}>
-        <Pressable style={{flex:1}} onPressIn={()=> pressLeft(5)}>
-          <View style={styles.left}></View>
+        
+        <Pressable style={{flex:1}} onPressIn={()=> pressLeft(1)}>
+          <View style={styles.left}>
+            <Text>Left</Text>
+          </View>
         </Pressable>
-        <Pressable style={{flex:1}} onPressIn={()=> pressLeft(4)}>
-          <View style={styles.left}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPressIn={()=> pressLeft(3)}>
-          <View style={styles.left}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPressIn={()=> pressLeft(2)}>
-          <View style={styles.left}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressLeft(1)}>
-          <View style={styles.left}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressRight(1)}>
-          <View style={styles.right}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressRight(2)}>
-          <View style={styles.right}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressRight(3)}>
-          <View style={styles.right}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressRight(4)}>
-          <View style={styles.right}></View>
-        </Pressable>
-        <Pressable style={{flex:1}} onPress={()=> pressRight(5)}>
-          <View style={styles.right}></View>
-        </Pressable>
-      </View>
-      <View style={{flex: 1, width:"100%", flexDirection:"row"}}>
-        <Pressable style={{flex:1}}>
-          <View style={styles.forward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.forward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.forward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.forward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.forward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.backward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.backward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.backward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.backward}></View>
-        </Pressable>
-        <Pressable style={{flex:1}}>
-          <View style={styles.backward}></View>
+
+        <Pressable style={{flex:1}} onPressIn={()=> pressRight(1)}>
+          <View style={styles.right}>
+          <Text>Right</Text>
+          </View>
         </Pressable>
         
       </View>
-    </View>
+      <View style={{flex: 1, width:"100%", flexDirection:"row"}}>
+        <Pressable style={{flex:1}} onPressIn={() => pressBackward(1)}>
+          <View style={styles.backward}>
+            <Text>Backward</Text>
+          </View>
+        </Pressable>
+        
+        <Pressable style={{flex:1}} onPressIn={() => pressForward(1)}>
+          <View style={styles.forward}>
+          <Text>Forward</Text>
+          </View>
+        </Pressable>
+        
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -103,23 +67,31 @@ const styles = StyleSheet.create({
     backgroundColor: "dodgerblue", 
     borderWidth: 1, 
     width:"100%",
+    alignItems: "center",
+    justifyContent: "center"
   },
   right: {
     flex: 1, 
     backgroundColor: "aliceblue", 
     borderWidth: 1, 
     width:"100%",
+    alignItems: "center",
+    justifyContent: "center"
   },
   forward: {
     flex: 1, 
     backgroundColor: "royalblue", 
     borderWidth: 1, 
     height:"100%",
+    alignItems: "center",
+    justifyContent: "center"
   },
   backward: {
     flex: 1, 
     backgroundColor: "steelblue", 
     borderWidth: 1, 
     height:"100%",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
