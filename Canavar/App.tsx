a550555852720a48d6748c524ca42a1980910df8
Salih Bektaş <sbektas@gtu.edu.tr>
2023-01-22 23:37:17 +0300

    add react navigation

--- Canavar/App.tsx
+++ Canavar/App.tsx
@@ -1,10 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Controller from './src/screens/Controller/Controller';
+import Scan from './src/screens/Scan/Scan';
 
 
+const Stack = createNativeStackNavigator();
 
-export default function App(){
-
-  return(
-    <></>
-  )
-
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} >
+        <Stack.Screen name="Controller" component={Controller} options={{orientation: 'landscape'}} />
+        <Stack.Screen name="Scan" component={Scan} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
