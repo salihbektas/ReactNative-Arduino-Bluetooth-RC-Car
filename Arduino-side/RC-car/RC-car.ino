ae3811cdbff30caf803e653e2494a829cf39cd2a
Salih Bektaş <sbektas@gtu.edu.tr>
2023-02-12 23:55:57 +0300

    added reading layout to arduino side

--- Arduino-side/RC-car/RC-car.ino
+++ Arduino-side/RC-car/RC-car.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial bt_connection(7,6);
-char data;
+char data1, data2, d1, d2;
+int i = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -11,8 +12,19 @@ void setup() {
 }
 
 void loop() {
-  if(bt_connection.available()){
-    data = bt_connection.read();
-    Serial.print(data);
+  while(bt_connection.available()){
+    ++i;
+    if(i % 2 == 0)
+      data1 = bt_connection.read();
+    else
+      data2 = bt_connection.read();
   }
+  if(data1 != d1 || data2 != d2){
+    Serial.print(data1);
+    Serial.println(data2);
+    Serial.flush();
+    d1 = data1;
+    d2 = data2;
+  }
+
 }
