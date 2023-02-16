bb7fc77c9a5f5283ff5d259d9032cbe7a2bd9fdb
Salih Bektaş <sbektas@gtu.edu.tr>
2023-02-16 23:30:50 +0300

    add car control code

--- Arduino-side/RC-car/RC-car.ino
+++ Arduino-side/RC-car/RC-car.ino
@@ -1,30 +1,81 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial bt_connection(7,6);
-char data1, data2, d1, d2;
-int i = 0;
+int throttle, steering, data;
+bool end = false;
+
+int speed[4] = {0, 120, 185, 255};
+
 
 void setup() {
   Serial.begin(9600);
   bt_connection.begin(9600);
   Serial.println("start");
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 
 }
 
 void loop() {
-  while(bt_connection.available()){
-    ++i;
-    if(i % 2 == 0)
-      data1 = bt_connection.read();
-    else
-      data2 = bt_connection.read();
+  if(bt_connection.available()){
+    if(bt_connection.peek() == 43){
+      end = true;
+    }
+    if(!end){
+      data = bt_connection.read();
+      throttle = ((data - 44) % 7) -3;
+      steering = ((data - 44) / 7) -3;
+    }
+    else{
+      Serial.print((char)bt_connection.read());
+    }
   }
-  if(data1 != d1 || data2 != d2){
-    Serial.print(data1);
-    Serial.println(data2);
-    Serial.flush();
-    d1 = data1;
-    d2 = data2;
+
+  int leftIndex, rightIndex;
+
+  if(throttle >= 0){
+    leftIndex = throttle + steering;
+    rightIndex = throttle - steering;
   }
+  else{
+    leftIndex = throttle - steering;
+    rightIndex = throttle + steering;
+  }
+
+  if(leftIndex < 0){
+    leftIndex *= -1;
+    digitalWrite(8, LOW);
+    digitalWrite(12, HIGH);
+  }
+  else{
+    digitalWrite(8, HIGH);
+    digitalWrite(12, LOW);
+  }
+    
+  if(rightIndex < 0){
+    rightIndex *= -1;
+    digitalWrite(9, LOW);
+    digitalWrite(13, HIGH);
+  }
+  else{
+    digitalWrite(9, HIGH);
+    digitalWrite(13, LOW);
+  }
+
+  if(leftIndex < 0)
+    leftIndex = 0;
+  if(leftIndex > 3)
+    leftIndex = 3;
+  if(rightIndex < 0)
+    rightIndex = 0;
+  if(rightIndex > 3)
+    rightIndex = 3;
+
+  analogWrite(10, speed[leftIndex]);
+  analogWrite(11, speed[rightIndex]);
 
 }
