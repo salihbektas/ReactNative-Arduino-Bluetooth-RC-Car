d6b060d74a56a2b96a548810407f7f340f037b9a
Salih Bektaş <sbektas@gtu.edu.tr>
2023-03-14 22:35:31 +0300

    feat: added splash screen for android

--- Canavar/App.tsx
+++ Canavar/App.tsx
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import Controller from './src/screens/Controller/Controller';
 import Scan from './src/screens/Scan/Scan';
 import { RootStackParamList } from './src/types';
@@ -8,6 +10,10 @@ import { RootStackParamList } from './src/types';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Scan' >
