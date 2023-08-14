5b7a726b2d2424c02e887bcf03c312b9fc1c78c0
Salih Bektaş <sbektas@gtu.edu.tr>
2023-08-14 23:57:44 +0300

    refactor: pin layout changed

--- Arduino-side/RC-car/RC-car.ino
+++ Arduino-side/RC-car/RC-car.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial bt_connection(7,6);
+SoftwareSerial bt_connection(5,4);
 int throttle, steering, data, junk = 0;
 bool end = false;
 
@@ -11,12 +11,12 @@ void setup() {
   Serial.begin(9600);
   bt_connection.begin(9600);
   Serial.println("start");
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
 
 }
 
@@ -24,6 +24,7 @@ void loop() {
   if(bt_connection.available()){
     if(bt_connection.peek() == 43){
       end = true;
+      Serial.println("end");
     }
     if(!end){
       data = bt_connection.read();
@@ -60,21 +61,21 @@ void loop() {
   if(leftIndex < 0){
     leftIndex *= -1;
     digitalWrite(8, LOW);
-    digitalWrite(12, HIGH);
+    digitalWrite(7, HIGH);
   }
   else{
     digitalWrite(8, HIGH);
-    digitalWrite(12, LOW);
+    digitalWrite(7, LOW);
   }
     
   if(rightIndex < 0){
     rightIndex *= -1;
-    digitalWrite(9, LOW);
-    digitalWrite(13, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
   }
   else{
-    digitalWrite(9, HIGH);
-    digitalWrite(13, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
   }
 
   if(leftIndex < 0)
@@ -86,7 +87,7 @@ void loop() {
   if(rightIndex > 3)
     rightIndex = 3;
 
-  analogWrite(10, speed[leftIndex]);
+  analogWrite(6, speed[leftIndex]);
   analogWrite(11, speed[rightIndex]);
 
 }
